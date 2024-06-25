241c2846be8fdf50e40be184bf0e72993774c9b2
unknown <vibertexyt@gmail.com>
2024-06-24 19:35:59 -0700

    added calendar

---
+++ Internship/Components/NextJS/components/app/weather-component/page.js
@@ -0,0 +1,17 @@
+'use client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Calendar from '../components/Calendar';
+
+const MainPage = () => {
+    return (
+        <div>
+            <h1>The 7 Wonders of the World</h1>
+            <Calendar></Calendar>
+        </div>
+    );
+};
+
+export default MainPage;
+
+
